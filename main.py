bbc8534f3222b541d0e44e92a553e55826740513
Ulyana Tarasova <tarasova.ulya@gmail.com>
2023-10-20 23:52:44 +0300

    add requirements, base template, checking the functional of fastapi

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Response
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
+app.mount("/about", StaticFiles(directory="templates"), name="about.html")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    data = 'Hello basic page'
+    return RedirectResponse("/hello/{name}")
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    data = "That's new page for something"
+    return Response(content=data, media_type='text/html')
