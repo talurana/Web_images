9bc5a390f20cd6eff73dda3747af603b56fddf30
Ulyana Tarasova <tarasova.ulya@gmail.com>
2023-11-05 17:41:50 +0300

    add router

--- scr/main.py
+++ scr/main.py
@@ -1,38 +1,18 @@
-from fastapi import FastAPI, Depends
-from fastapi_users import FastAPIUsers
+from fastapi import FastAPI
 
 from .authen.base_config import auth_backend, fastapi_users
-from scr.authen.models import User
-from .authen.manager import get_user_manager
 from .authen.schemas import UserRead, UserCreate
+
+from .image.router import router as router_operation
 
 app = FastAPI(
     title="Web Images"
 )
 
 
-fastapi_users = FastAPIUsers[User, int](
-    get_user_manager,
-    [auth_backend],
-)
-
-# app.mount("/index", StaticFiles(directory="templates", html=True))
-#
-#
-# @app.get("/root")
-# async def root():
-#     return RedirectResponse("/hello/{name}")
-#
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     data = "That's new page for something"
-#     return Response(content=data, media_type='text/html')
-#
-
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
+    prefix="/auth",
     tags=["auth"],
 )
 
@@ -42,9 +22,6 @@ app.include_router(
     tags=["auth"],
 )
 
-current_user = fastapi_users.current_user()
-
-
-@app.get("/protected-route")
-def protected_route(user: User = Depends(current_user)):
-    return f"Hello, {user.username}"
+app.include_router(
+    router_operation
+)
