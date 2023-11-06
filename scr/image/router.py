da1246b3fc86f102515f5b735e2959948ddac8f4
Tarasova Uliana <118310525+talurana@users.noreply.github.com>
2023-11-06 14:41:33 +0300

    fixed total_nb without filter

--- scr/image/router.py
+++ scr/image/router.py
@@ -21,8 +21,11 @@ async def get_image(
     query = select(image)
     if filter_str:
         query = query.where(image.c.file_name.ilike(f"%{filter_str}%"))
+        total_nb_query = select(func.count()).select_from(image).where(image.c.file_name.ilike(f"%{filter_str}%"))
 
-    total_nb_query = select(func.count()).select_from(image).where(image.c.file_name.ilike(f"%{filter_str}%"))
+    else:
+        total_nb_query = select(func.count()).select_from(image)
+
     total_nb = await session.scalar(total_nb_query)
 
     query = query.limit(limit).offset(offset)
