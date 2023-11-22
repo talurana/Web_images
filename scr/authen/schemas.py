732c6d24b7865e63bbfd3edf586b2461c93b3434
Tarasova Uliana <118310525+talurana@users.noreply.github.com>
2023-11-22 23:17:03 +0300

    add name, surname, middle name

--- scr/authen/schemas.py
+++ scr/authen/schemas.py
@@ -5,7 +5,9 @@ from fastapi_users import schemas
 
 class UserRead(schemas.BaseUser[int]):
     email: str
-    username: str
+    name: str
+    surname: str
+    middle_name: str
     id: int
     role_id: int
     is_active: bool = True
@@ -18,13 +20,14 @@ class UserRead(schemas.BaseUser[int]):
 
 class UserCreate(schemas.BaseUserCreate):
     email: str
-    username: str
+    name: str
+    surname: str
+    middle_name: str
     password: str
-    role_id: int
-    is_active: Optional[bool] = True
-    is_superuser: Optional[bool] = False
-    is_verified: Optional[bool] = False
 
 
-class UserUpdate(schemas.BaseUserUpdate):
-    username: str
+class UserUpdate(schemas.BaseUser):
+    email: str
+    name: str
+    surname: str
+    middle_name: str
